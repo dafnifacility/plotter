e10baa76fa2d56b7b8fc48c7adaef3025a523a9a
James Hannah <james.hannah@stfc.ac.uk>
2021-06-15 14:29:28 +0100

    try ssr mode

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,6 +14,7 @@ export default {
    ** See https://nuxtjs.org/api/configuration-mode
    */
   mode: 'spa',
+  ssr: false,
   /*
    ** Nuxt target
    ** See https://nuxtjs.org/api/configuration-target
