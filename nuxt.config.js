9154c600aa46c48a2691a3c0d04d0561c177da00
Rose Dickinson <rose.dickinson@stfc.ac.uk>
2021-06-22 10:16:56 +0100

    fix: ExtractCSS true only locally

--- nuxt.config.js
+++ nuxt.config.js
@@ -104,7 +104,7 @@ export default {
    ** See https://nuxtjs.org/api/configuration-build/
    */
   build: {
-    extractCSS: true,
+    extractCSS: process.env.NODE_ENV === 'development',
     publicPath: process.env.NODE_ENV === 'development' ? '/_nuxt' : './_nuxt/',
     extend(config) {
       // Run ESLint on save
